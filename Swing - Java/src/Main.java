80f8de1daf1bf79663f580864bf35b2109f1ed84
JayReeMarine <lee010518@gmail.com>
2025-03-16 23:49:52 +1100

    part 6,7 java swing

--- Swing - Java/src/Main.java
+++ Swing - Java/src/Main.java
@@ -110,64 +110,121 @@ public class Main {
 
         // BorderLayout = a layout manager that manages components in five areas: north, south, west, east, center
 
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500,500);
+//
+//        frame.setLayout(new BorderLayout(10,20));
+//        frame.setVisible(true);
+//
+//        JPanel panel1 = new JPanel();
+//        JPanel panel2 = new JPanel();
+//        JPanel panel3 = new JPanel();
+//        JPanel panel4 = new JPanel();
+//        JPanel panel5 = new JPanel();
+//
+//        panel1.setBackground(Color.red);
+//        panel2.setBackground(Color.green);
+//        panel3.setBackground(Color.yellow);
+//        panel4.setBackground(Color.magenta);
+//        panel5.setBackground(Color.blue);
+//
+//        panel1.setPreferredSize(new Dimension(100,100));
+//        panel2.setPreferredSize(new Dimension(100,100));
+//        panel3.setPreferredSize(new Dimension(100,100));
+//        panel4.setPreferredSize(new Dimension(100,100));
+//        panel5.setPreferredSize(new Dimension(100,100));
+//
+//        //----- sub panels -----
+//
+//        JPanel panel6 = new JPanel();
+//        JPanel panel7 = new JPanel();
+//        JPanel panel8 = new JPanel();
+//        JPanel panel9 = new JPanel();
+//        JPanel panel10 = new JPanel();
+//
+//        panel6.setBackground(Color.black);
+//        panel7.setBackground(Color.darkGray);
+//        panel8.setBackground(Color.gray);
+//        panel9.setBackground(Color.lightGray);
+//        panel10.setBackground(Color.white);
+//
+//        panel5.setLayout(new BorderLayout());
+//
+//        panel6.setPreferredSize(new Dimension(50,50));
+//        panel7.setPreferredSize(new Dimension(50,50));
+//        panel8.setPreferredSize(new Dimension(50,50));
+//        panel9.setPreferredSize(new Dimension(50,50));
+//        panel10.setPreferredSize(new Dimension(50,50));
+//
+//        panel5.add(panel6, BorderLayout.NORTH);
+//        panel5.add(panel7, BorderLayout.SOUTH);
+//        panel5.add(panel8, BorderLayout.WEST);
+//        panel5.add(panel9, BorderLayout.EAST);
+//        panel5.add(panel10, BorderLayout.CENTER);
+//
+//        frame.add(panel1, BorderLayout.NORTH);
+//        frame.add(panel2, BorderLayout.WEST);
+//        frame.add(panel3, BorderLayout.EAST);
+//        frame.add(panel4, BorderLayout.SOUTH);
+//        frame.add(panel5, BorderLayout.CENTER);
+
+        //--------------------------------------------------------------------------------
+        //part six
+
+        // FlowLayout = a layout manager that places components in a row, sized at their preferred size.
+        //              If the horizontal space in the container is too small,
+        //              the FlowLayout class uses the next available row.
+
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500,500);
+//        frame.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+//
+//        JPanel panel = new JPanel();
+//        panel.setPreferredSize(new Dimension(100,250));
+//        panel.setBackground(Color.lightGray);
+//        panel.setLayout(new FlowLayout());
+//
+//
+//
+//        panel.add(new JButton("1"));
+//        panel.add(new JButton("2"));
+//        panel.add(new JButton("3"));
+//        panel.add(new JButton("4"));
+//        panel.add(new JButton("5"));
+//        panel.add(new JButton("6"));
+//        panel.add(new JButton("7"));
+//        panel.add(new JButton("8"));
+//        panel.add(new JButton("9"));
+//
+//        frame.add(panel);
+//        frame.setVisible(true);
+
+        //--------------------------------------------------------------------------------
+        //part seven
+
+        // GridLayout = a layout manager that lays out a container's components in a rectangular grid
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
+        frame.setLayout(new GridLayout(3,3,10,10));
 
-        frame.setLayout(new BorderLayout(10,20));
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+
+
         frame.setVisible(true);
 
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
-        JPanel panel3 = new JPanel();
-        JPanel panel4 = new JPanel();
-        JPanel panel5 = new JPanel();
 
-        panel1.setBackground(Color.red);
-        panel2.setBackground(Color.green);
-        panel3.setBackground(Color.yellow);
-        panel4.setBackground(Color.magenta);
-        panel5.setBackground(Color.blue);
-
-        panel1.setPreferredSize(new Dimension(100,100));
-        panel2.setPreferredSize(new Dimension(100,100));
-        panel3.setPreferredSize(new Dimension(100,100));
-        panel4.setPreferredSize(new Dimension(100,100));
-        panel5.setPreferredSize(new Dimension(100,100));
-
-        //----- sub panels -----
-
-        JPanel panel6 = new JPanel();
-        JPanel panel7 = new JPanel();
-        JPanel panel8 = new JPanel();
-        JPanel panel9 = new JPanel();
-        JPanel panel10 = new JPanel();
-
-        panel6.setBackground(Color.black);
-        panel7.setBackground(Color.darkGray);
-        panel8.setBackground(Color.gray);
-        panel9.setBackground(Color.lightGray);
-        panel10.setBackground(Color.white);
-
-        panel5.setLayout(new BorderLayout());
-
-        panel6.setPreferredSize(new Dimension(50,50));
-        panel7.setPreferredSize(new Dimension(50,50));
-        panel8.setPreferredSize(new Dimension(50,50));
-        panel9.setPreferredSize(new Dimension(50,50));
-        panel10.setPreferredSize(new Dimension(50,50));
-
-        panel5.add(panel6, BorderLayout.NORTH);
-        panel5.add(panel7, BorderLayout.SOUTH);
-        panel5.add(panel8, BorderLayout.WEST);
-        panel5.add(panel9, BorderLayout.EAST);
-        panel5.add(panel10, BorderLayout.CENTER);
-
-        frame.add(panel1, BorderLayout.NORTH);
-        frame.add(panel2, BorderLayout.WEST);
-        frame.add(panel3, BorderLayout.EAST);
-        frame.add(panel4, BorderLayout.SOUTH);
-        frame.add(panel5, BorderLayout.CENTER);
 
     }
 }
