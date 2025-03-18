afc0de2bf6c03dd5375051c0d2a0ec56a3d5fdab
JayReeMarine <lee010518@gmail.com>
2025-03-18 14:13:47 +1100

    Java Swing part 8

--- Swing - Java/src/Main.java
+++ Swing - Java/src/Main.java
@@ -206,22 +206,56 @@ public class Main {
 
         // GridLayout = a layout manager that lays out a container's components in a rectangular grid
 
-        JFrame frame = new JFrame();
+//        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500,500);
+//        frame.setLayout(new GridLayout(3,3,10,10));
+//
+//        frame.add(new JButton("1"));
+//        frame.add(new JButton("2"));
+//        frame.add(new JButton("3"));
+//        frame.add(new JButton("4"));
+//        frame.add(new JButton("5"));
+//        frame.add(new JButton("6"));
+//        frame.add(new JButton("7"));
+//        frame.add(new JButton("8"));
+//        frame.add(new JButton("9"));
+//
+//
+//        frame.setVisible(true);
+
+        //--------------------------------------------------------------------------------
+        //part eight
+
+        // JLayeredPane = Swing container that provides a third dimension for positioning components
+
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.red);
+        label1.setBounds(50,50,200,200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.green);
+        label2.setBounds(100,100,200,200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.blue);
+        label3.setBounds(150,150,200,200);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,500,500);
+
+        layeredPane.add(label1, Integer.valueOf(0));
+        layeredPane.add(label2, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(label3, JLayeredPane.DRAG_LAYER);
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
-        frame.setLayout(new GridLayout(3,3,10,10));
-
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
-
-
+        frame.setLayout(null);
         frame.setVisible(true);
 
 
