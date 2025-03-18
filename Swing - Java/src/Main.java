a56bb74212955a2b6a751337f791276d919f6454
JayReeMarine <lee010518@gmail.com>
2025-03-18 14:40:40 +1100

    Java Swing part 10

--- Swing - Java/src/Main.java
+++ Swing - Java/src/Main.java
@@ -261,8 +261,32 @@ public class Main {
         //--------------------------------------------------------------------------------
         //part nine
 
-        LaunchPage launchPage = new LaunchPage();
+//        LaunchPage launchPage = new LaunchPage();
 
+        //--------------------------------------------------------------------------------
+        //part ten
 
+        //JOptionPane = pop up a standard dialog box that prompts users for a value or informs them of something.
+
+//        JOptionPane.showMessageDialog(null, "This is some useful information.", "Title", JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "This is really useful information.", "Title", JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Really?", "Title", JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "You've been warned", "Title", JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Call tech support!", "Title", JOptionPane.ERROR_MESSAGE);
+
+        //System.out.println(JOptionPane.showConfirmDialog(null, "Bro, do you even code?", "This is my title", JOptionPane.YES_NO_CANCEL_OPTION));
+
+//        String name = JOptionPane.showInputDialog("What is your name?:  ");
+//        System.out.println("Hello " + name);
+
+        String[] responses = {"No, you're awesome!", "Thank you!", "*blush*"};
+        JOptionPane.showOptionDialog(null,
+                "This is my custom dialog",
+                "secret message",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                responses,
+                0);
     }
 }
