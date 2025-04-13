85b33c0aab072ebcfd8968aa685cf285d5406524
JayReeMarine <lee010518@gmail.com>
2025-04-13 17:13:25 +1000

    modification on leetcode question

--- LeetCode/Array + String/Q56.Merge Intervals.py
+++ LeetCode/Array + String/Q56.Merge Intervals.py
@@ -1,5 +1,6 @@
 #https://leetcode.com/problems/merge-intervals/description/
 
+from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
@@ -16,3 +17,12 @@ class Solution:
                 else:
                     res.append(intervals[i])
         return res
+    
+
+
+# Test case
+if __name__ == "__main__":
+    intervals = [[1,3],[2,6],[8,10],[15,18]]
+    sol = Solution()
+    print(sol.merge(intervals))  # Output: [[1,6],[8,10],[15,18]]
+    # Test case with overlapping intervals
--- LeetCode/Hash Table/Q621.Task Scheduler.py
+++ LeetCode/Hash Table/Q621.Task Scheduler.py
@@ -1,3 +1,5 @@
+#https://leetcode.com/problems/task-scheduler/
+
 # class Solution:
 #     def leastInterval(self, tasks: list[str], n: int) -> int:
 #         freq = {}
