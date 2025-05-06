7f9c2f535a0ddd8353ba2d81233a7296a2c3d15f
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-05-06 19:03:38 +1000

    Create Q509. Fibonacci Number.py

---
+++ LeetCode/Dynamic Programming/Q509. Fibonacci Number.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/fibonacci-number/submissions/1626192322/
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n  == 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        return self.fib(n-1) + self.fib(n-2)
