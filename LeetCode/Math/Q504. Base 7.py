a4abe1f9369f9467ac67edf2a764183ab4a8394c
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-06-05 12:42:39 +1000

    Create Q504. Base 7.py

---
+++ LeetCode/Math/Q504. Base 7.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/base-7/
+
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        negative = num < 0
+
+        num = abs(num)
+        res = ""
+
+        while num > 0:
+            res = str(num % 7) + res
+            num  = num //7
+        
+        return "-" + res if negative else res
+    
