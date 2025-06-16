771bd1577be088cc3549a37719dca80f87e75a68
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-06-16 20:57:04 +1000

    Create Q3065. Minimum Operations to Exceed Threshold Value I.py

---
+++ LeetCode/Array + String/Q3065. Minimum Operations to Exceed Threshold Value I.py
@@ -0,0 +1,10 @@
+from collections import List
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in range(len(nums)):
+            if nums[i] < k:
+                res += 1
+        
+        return res
