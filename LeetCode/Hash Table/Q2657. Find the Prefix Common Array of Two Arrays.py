e06141778d99f1da062ef3f80f122be19ce58805
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-06-26 12:23:33 +1000

    Update Q2657. Find the Prefix Common Array of Two Arrays.py

--- LeetCode/Hash Table/Q2657. Find the Prefix Common Array of Two Arrays.py
+++ LeetCode/Hash Table/Q2657. Find the Prefix Common Array of Two Arrays.py
@@ -1,16 +1,45 @@
-from collections import Counter, List
+#https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/
 
+from collections import Counter, List
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        ctr = Counter()
+        seen_a = set()
+        seen_b = set()
+        seen_both = set()
         res = []
+        count = 0
+
         for i in range(len(A)):
-            freq = 0
-            ctr.update([A[i], B[i]])
-            for k, v in ctr.items():
-                if v %2 == 0:
-                    freq += 1
-            res.append(freq)
+            a, b = A[i], B[i]
+
+            if a == b and a not in seen_both:
+                count += 1
+                seen_both.add(a)
+
+            else:
+                if a in seen_b and a not in seen_both:
+                    count += 1
+                    seen_both.add(a)
+
+                if b in seen_a and b not in seen_both:
+                    count += 1
+                    seen_both.add(b)
+            
+            seen_a.add(a)
+            seen_b.add(b)
+            res.append(count)
         
         return res
+        
+        # ctr = Counter()
+        # res = []
+        # for i in range(len(A)):
+        #     freq = 0
+        #     ctr.update([A[i], B[i]])
+        #     for k, v in ctr.items():
+        #         if v %2 == 0:
+        #             freq += 1
+        #     res.append(freq)
+        
+        # return res
 
