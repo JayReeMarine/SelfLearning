fa23bfc906b9dba849176719f6d8059c4723d1c0
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-08-01 10:30:34 +1000

    Update Q279. Perfect Squares.py

--- LeetCode/Dynamic Programming/Q279. Perfect Squares.py
+++ LeetCode/Dynamic Programming/Q279. Perfect Squares.py
@@ -2,29 +2,46 @@
 
 import math
 from typing import List
-
+           
 class Solution:
     def numSquares(self, n: int) -> int:
-        memo = {}
-        def dp(n):
-            if n <= 1:
-                return n
-            if n in memo:
-                return memo[n]
-            if math.isqrt(n) ** 2 == n:
-                memo[n] = 1
-                return 1
+        dp = [n] * (n + 1)
+        dp[0] = 0
+
+        for target in range(1, n + 1):
+            for s in range(1, target + 1):
+                square = s * s
+                if target < square:
+                    break
+                dp[target] = min(dp[target], 1 + dp[target - square])
+        
+        return dp[n]
+
+
+        # memo = {}
+        # def dp(n):
+        #     if n <= 1:
+        #         return n
+        #     if n in memo:
+        #         return memo[n]
+        #     if math.isqrt(n) ** 2 == n:
+        #         memo[n] = 1
+        #         return 1
             
-            res = float('inf')
-            for i in range(1, math.isqrt(n) + 1):
-                res = min(res, dp(n- i*i) + 1)
+        #     res = float('inf')
+        #     for i in range(1, math.isqrt(n) + 1):
+        #         res = min(res, dp(n- i*i) + 1)
             
-            memo[n] = res
-            return res
+        #     memo[n] = res
+        #     return res
             
 
-        return dp(n)
+        # return dp(n)
             
+            
+
+
+
             
 
 
