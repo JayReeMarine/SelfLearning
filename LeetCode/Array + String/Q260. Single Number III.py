71447d135009d4e9c095fa33a3bb90dfb885c056
JayReeMarine <140177619+JayReeMarine@users.noreply.github.com>
2025-08-30 18:58:11 +1000

    Create Q260. Single Number III.py

---
+++ LeetCode/Array + String/Q260. Single Number III.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/single-number-iii/description/
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        res = set()
+
+        for num in nums:
+            if num in res:
+                res.remove(num)
+            else:
+                res.add(num)
+
+        return list(res)
