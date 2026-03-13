48714e483a8b31d342ba44976327a2e1087f98a3
JayReeMarine <lee010518@gmail.com>
2026-03-13 23:09:52 +1100

    prep p4

--- Languages/C++/FIT1045/W2/P4/dynamic_img.cpp
+++ Languages/C++/FIT1045/W2/P4/dynamic_img.cpp
@@ -12,12 +12,12 @@ int main()
     user_input = read_line();
     rotation = to_double(user_input);
 
-    open_window("Image Drawing", 500, 300);
+    open_window("Image Drawing", 1000, 600);
 
     tree = load_bitmap(IMAGE_NAME, "tree.png");
 
-    draw_bitmap(tree, 10, 20, option_scale_bmp(0.3, 0.3));
-    draw_bitmap(IMAGE_NAME, 220, 20, option_scale_bmp(0.3, 0.3, option_rotate_bmp(rotation)));
+    draw_bitmap(tree, 10, 20);
+    draw_bitmap(IMAGE_NAME, 220, 50, option_rotate_bmp(rotation));
     refresh_screen();
     delay(5000);
 
